e9ec55b0864847fcb37d42c745865a540728e465
Alex Alikiotis <alexalikiotis5@gmail.com>
2017-05-24 17:13:31 +0300

    Create database class

---
+++ src/services/Database.java
@@ -0,0 +1,29 @@
+package services;
+
+import java.sql.Connection;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class Database {
+	
+	static public Connection getConnection(){
+		
+		try{
+			InitialContext cxt = new InitialContext();
+
+			DataSource ds = (DataSource) cxt.lookup( "java:/comp/env/jdbc/postgres" );
+			
+			Connection conn = ds.getConnection();
+			
+			return conn;
+
+		}catch(Exception e){
+			
+			System.out.println(e.getMessage());
+			
+			return null;
+		}
+		
+	}
+	
+}
