2b90afdf5eab66fefbb0d003668781a7467627fb
Michael Skaltsounakis <mi.skal2467@gmail.com>
2017-06-15 19:15:09 +0300

    Update admin.java and admin.jsp

---
+++ src/admin/Admin.java
@@ -0,0 +1,34 @@
+package admin;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name="Admin_dashboard", urlPatterns={"/admin/dashboard"})
+public class Admin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+    public Admin() {
+        super();
+
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		request.getRequestDispatcher("/JSP/Admin/admin.jsp").forward(request, response);
+		
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		doGet(request, response);
+	}
+
+}
